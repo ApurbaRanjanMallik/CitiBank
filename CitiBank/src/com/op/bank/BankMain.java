2b11c0e53323392d5f17c7137a416564cd5017fc
Apurba <mallikapurbaranjan@gmail.com;>
2023-04-23 01:43:20 +0530

    2nd commit by Apurba

--- CitiBank/src/com/op/bank/BankMain.java
+++ CitiBank/src/com/op/bank/BankMain.java
@@ -3,7 +3,8 @@ package com.op.bank;
 public class BankMain {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to CitiBank");
+		System.out.println("Welcome to CitiBank"); 
+		System.out.println("Please open an account and Enjoy your transactions");
 	}
 
 }
---
+++ CitiBank/src/com/op/bank/BankRepo.java
@@ -0,0 +1,7 @@
+package com.op.bank;
+
+public class BankRepo {
+	public static void main(String[] args) {
+		System.out.println("This is my BankRepo class");
+	}
+}
